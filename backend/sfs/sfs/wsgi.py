1fdf81edaf142385faa21a219a1f47cc0a622286
Loc Nguyen <loc.nguyen@saritasa.com>
2023-03-31 13:40:28 +0700

    Rename: Backend -> backend, SFS -> sfs

--- backend/sfs/sfs/wsgi.py
+++ backend/sfs/sfs/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for SFS project.
+WSGI config for sfs project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SFS.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sfs.settings')
 
 application = get_wsgi_application()
