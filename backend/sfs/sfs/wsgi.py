86e7dfc8e0975396bf85ec3d84f66e4017768c8c
Loc Nguyen <loc.nguyen@saritasa.com>
2023-03-31 22:21:33 +0700

    "Refactor" Django codes

--- backend/sfs/sfs/wsgi.py
+++ backend/sfs/sfs/wsgi.py
@@ -1,5 +1,4 @@
-"""
-WSGI config for sfs project.
+"""WSGI config for sfs project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +10,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sfs.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sfs.settings")
 
 application = get_wsgi_application()
