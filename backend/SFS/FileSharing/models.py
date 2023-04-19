ea65cb249d0a00bdd929cc21af4b76cff36f8ae8
Loc Nguyen <loc.nguyen@saritasa.com>
2023-04-19 16:45:06 +0700

    Delete redundant files

--- backend/SFS/FileSharing/models.py
+++
@@ -1,5 +0,0 @@
-# from django.db import models
-
-"""Docstring."""
-
-# Create your models here.
--- backend/server_run.sh
+++ backend/server_run.sh
@@ -5,16 +5,11 @@ pyenv activate secure-file-sharing-server
 # pip install -r requirements/development.txt
 
 # Start the first process
-<<<<<<< Updated upstream
-
-python ./sfs/manage.py livereload &
-=======
 # python ./sfs/manage.py livereload &
 
 python ./sfs/manage.py makemigrations
 
 python ./sfs/manage.py migrate
->>>>>>> Stashed changes
 
 # Start the second process
 python ./sfs/manage.py runserver
