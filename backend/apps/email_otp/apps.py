9eb229e55cb618c750dfd72982e80c9c70715812
Loc Nguyen <loc.nguyen@saritasa.com>
2023-04-24 23:01:25 +0700

    Initialize Django apps

---
+++ backend/apps/email_otp/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class EmailOtpConfig(AppConfig):
+    """App config for email_otp app."""
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.email_otp"
---
+++ backend/apps/users/apps.py
@@ -0,0 +1,8 @@
+# pylint: disable=C0114
+from django.apps import AppConfig
+
+
+class UsersConfig(AppConfig):
+    """App config for users app."""
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.users"
