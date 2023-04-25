1292c2bf4e75771ea5f553cc30e988c4eea82a08
Loc Nguyen <loc.nguyen@saritasa.com>
2023-04-25 11:34:29 +0700

    Update check coding styles

--- backend/check_coding_styles.sh
+++ backend/check_coding_styles.sh
@@ -1,8 +1,6 @@
 set -xe
 isort .
-flake8 . --show-source
+# flake8 . --show-source
 
 # pylint ignore type hints
-pylint ./apps/ --disable=C0114,C0115,C0116 --ignore=types.py
-
-mypy ./apps/
+pylint ./apps/
