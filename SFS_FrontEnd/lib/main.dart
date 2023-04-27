e3ea08ece0def938aafffd335f0a8b029508da53
mekanican <boommerang73@gmail.com>
2023-04-28 02:35:03 +0700

    Init flutter w/ basic functionality

---
+++ SFS_FrontEnd/lib/main.dart
@@ -0,0 +1,242 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_login/flutter_login.dart';
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
+import 'package:flutter_chat_ui/flutter_chat_ui.dart';
+import 'package:http/http.dart' as http;
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:mime/mime.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:uuid/uuid.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SFS',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.teal,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;  
+
+  @override
+  Widget build(BuildContext context) {
+
+    Widget page;
+    switch(selectedIndex) {
+      case 0:
+        page = HomePage();
+        break;
+      case 1:
+        page = LoginPage();
+        break;
+      case 2:
+        page = FriendListPage();
+        break;
+      case 3:
+        page = ChatPage();
+        break;
+      default:
+        throw UnimplementedError("Not found");
+    }
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          body: Row(
+            children: [
+              getHorizontalNavigationBar(constraints),
+              Expanded(child: Container(
+                color: Colors.white,
+                child: page
+              ))
+            ],
+          ),
+        );
+      }
+    );
+  }
+  
+  SafeArea getHorizontalNavigationBar(BoxConstraints constraints) {
+    return SafeArea(child: NavigationRail(
+      destinations: const [
+        NavigationRailDestination(icon: Icon(Icons.home), label: Text("Home")),
+        NavigationRailDestination(icon: Icon(Icons.login), label: Text("Login")),
+        NavigationRailDestination(icon: Icon(Icons.supervised_user_circle), label: Text("List of friends")),
+        NavigationRailDestination(icon: Icon(Icons.chat), label: Text("Chat")),
+      ],
+      selectedIndex: selectedIndex,
+      onDestinationSelected: (value) {
+        setState(() {
+          selectedIndex = value;
+        });
+      },
+      extended: constraints.maxWidth >= 700,
+      backgroundColor: Colors.lime.shade300,
+      )
+    );
+  }
+  
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image(image: AssetImage("logo/logo_full.png"))
+        ],
+      ),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterLogin(
+      onLogin: (_){}, 
+      onRecoverPassword: (_){},
+      onSignup: (_){},
+      logo: AssetImage("logo/logo_icon.png"),
+      );
+  }
+}
+
+class FriendListPage extends StatefulWidget {
+  const FriendListPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _FriendListPageState();
+  }
+}
+
+class _FriendListPageState extends State<FriendListPage> {
+  static const _pageSize = 20;
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
+
+
+class ChatPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ChatPageState();
+  }
+
+}
+
+class _ChatPageState extends State<ChatPage> {
+  final List<types.Message> _messages = [];
+  final _user = const types.User(id: '82091008-a484-4a89-ae75-a22bf8d6f3ac');
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    body: Chat(
+          messages: _messages,
+          onSendPressed: _handleSendPressed,
+          onAttachmentPressed: _handleFileSelection,
+          user: _user,
+    ),
+  );
+
+  void _handleSendPressed(types.PartialText message) {
+    final textMessage = types.TextMessage(
+      author: _user,
+      createdAt: DateTime.now().millisecondsSinceEpoch,
+      id: randomString(),
+      text: message.text,
+    );
+
+    _addMessage(textMessage);
+    
+  }
+  String randomString() {
+    final random = Random.secure();
+    final values = List<int>.generate(16, (i) => random.nextInt(255));
+    return base64UrlEncode(values);
+  }
+  
+  void _addMessage(types.Message textMessage) {
+    setState(() {
+      _messages.insert(0, textMessage);
+    });
+  }
+
+  void _handleFileSelection() async {
+    final result = await FilePicker.platform.pickFiles(
+      type: FileType.any,
+    );
+
+    if (result != null && result.files.single.path != null) {
+      final message = types.FileMessage(
+        author: _user,
+        createdAt: DateTime.now().millisecondsSinceEpoch,
+        id: const Uuid().v4(),
+        mimeType: lookupMimeType(result.files.single.path!),
+        name: result.files.single.name,
+        size: result.files.single.size,
+        uri: result.files.single.path!,
+      );
+
+      _addMessage(message);
+    }
+  }
+}
