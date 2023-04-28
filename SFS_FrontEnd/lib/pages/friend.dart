3b17de009b8304f7784ed117a529de8cc588350c
mekanican <boommerang73@gmail.com>
2023-04-28 10:39:27 +0700

    Restructure flutter

---
+++ SFS_FrontEnd/lib/pages/friend.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+
+class FriendListPage extends StatefulWidget {
+  const FriendListPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _FriendListPageState();
+  }
+}
+
+class _FriendListPageState extends State<FriendListPage> {
+  static const _pageSize = 20;
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
