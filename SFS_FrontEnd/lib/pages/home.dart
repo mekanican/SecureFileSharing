f838f98ac3fac5d7be73f808153b57fca28248f2
mekanican <boommerang73@gmail.com>
2023-04-28 10:39:27 +0700

    Restructure flutter

---
+++ SFS_FrontEnd/lib/pages/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image(image: AssetImage("logo/logo_full.png"))
+        ],
+      ),
+    );
+  }
+}
