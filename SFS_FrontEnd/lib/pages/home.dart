1bcc9323c9802b7b531af108e6a86129abc22e03
mekanican <boommerang73@gmail.com>
2023-04-28 16:21:59 +0700

    Add sample code to download flutter

--- SFS_FrontEnd/lib/pages/home.dart
+++ SFS_FrontEnd/lib/pages/home.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sfs_frontend/helper/file_downloader.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // Example code to download file
+    getApplicationSupportDirectory().then((value) => downloadFile("https://www.google.com/robots.txt", "${value.path}/robots.txt"));
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
