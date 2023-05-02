04acccbeb35fc64d0771da15166b21e6391cd8d9
sangphamitus <phamphuocsang2834@gmail.com>
2023-05-02 12:00:11 +0700

    Resolve conflict

--- SFS_FrontEnd/lib/main.dart
+++ SFS_FrontEnd/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import 'package:provider/provider.dart';
 
-
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
@@ -12,7 +11,6 @@ import 'package:mime/mime.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:uuid/uuid.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
-import 'package:http/http.dart' as http;
 
 import 'package:sfs_frontend/pages/chat.dart';
 import 'package:sfs_frontend/pages/home.dart';
@@ -129,7 +127,6 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
@@ -140,23 +137,6 @@ class HomePage extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [Image(image: AssetImage("logo/logo_full.png"))],
       ),
-    );
-  }
-}
-
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
-
- 
-  @override
-  Widget build(BuildContext context) {
-    return FlutterLogin(
-      onLogin: (_) {},
-      onRecoverPassword: (_) {},
-      onSignup: (_)  {
-      
-      },
-      logo: AssetImage("logo/logo_icon.png"),
     );
   }
 }
@@ -177,68 +157,3 @@ class _FriendListPageState extends State<FriendListPage> {
     throw UnimplementedError();
   }
 }
-
-class ChatPage extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _ChatPageState();
-  }
-}
-
-class _ChatPageState extends State<ChatPage> {
-  final List<types.Message> _messages = [];
-  final _user = const types.User(id: '82091008-a484-4a89-ae75-a22bf8d6f3ac');
-  @override
-  Widget build(BuildContext context) => Scaffold(
-        body: Chat(
-          messages: _messages,
-          onSendPressed: _handleSendPressed,
-          onAttachmentPressed: _handleFileSelection,
-          user: _user,
-        ),
-      );
-
-  void _handleSendPressed(types.PartialText message) {
-    final textMessage = types.TextMessage(
-      author: _user,
-      createdAt: DateTime.now().millisecondsSinceEpoch,
-      id: randomString(),
-      text: message.text,
-    );
-
-    _addMessage(textMessage);
-  }
-
-  String randomString() {
-    final random = Random.secure();
-    final values = List<int>.generate(16, (i) => random.nextInt(255));
-    return base64UrlEncode(values);
-  }
-
-  void _addMessage(types.Message textMessage) {
-    setState(() {
-      _messages.insert(0, textMessage);
-    });
-  }
-
-  void _handleFileSelection() async {
-    final result = await FilePicker.platform.pickFiles(
-      type: FileType.any,
-    );
-
-    if (result != null && result.files.single.path != null) {
-      final message = types.FileMessage(
-        author: _user,
-        createdAt: DateTime.now().millisecondsSinceEpoch,
-        id: const Uuid().v4(),
-        mimeType: lookupMimeType(result.files.single.path!),
-        name: result.files.single.name,
-        size: result.files.single.size,
-        uri: result.files.single.path!,
-      );
-
-      _addMessage(message);
-    }
-  }
-}
-
