6aed434201161eceed39ec36a69b79420fd13571
mekanican <boommerang73@gmail.com>
2023-05-03 01:32:05 +0700

    Restructure (create 2 route for guess & logged in)

--- SFS_FrontEnd/lib/main.dart
+++ SFS_FrontEnd/lib/main.dart
@@ -2,20 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:provider/provider.dart';
 
-import 'package:flutter/services.dart' show rootBundle;
-import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
-import 'package:flutter_chat_ui/flutter_chat_ui.dart';
-import 'package:http/http.dart' as http;
-import 'package:intl/date_symbol_data_local.dart';
-import 'package:mime/mime.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:uuid/uuid.dart';
-import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
-
-import 'package:sfs_frontend/pages/chat.dart';
-import 'package:sfs_frontend/pages/home.dart';
-import 'package:sfs_frontend/pages/friend.dart';
-import 'package:sfs_frontend/pages/login.dart';
+import 'package:sfs_frontend/pages/GuestRoute/guest_route.dart';
 import 'package:sfs_frontend/services/user_state.dart';
 
 void main() {
@@ -44,85 +31,8 @@ class MyApp extends StatelessWidget {
           // is not restarted.
           primarySwatch: Colors.teal,
         ),
-        home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        home: const GuestRoute(title: 'Flutter Demo Home Page'),
       ),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  var selectedIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    Widget page;
-    switch (selectedIndex) {
-      case 0:
-        page = HomePage();
-        break;
-      case 1:
-        page = LoginPage();
-        break;
-      case 2:
-        page = FriendListPage();
-        break;
-      case 3:
-        page = ChatPage();
-        break;
-      default:
-        throw UnimplementedError("Not found");
-    }
-
-    return LayoutBuilder(builder: (context, constraints) {
-      return Scaffold(
-        body: Row(
-          children: [
-            getHorizontalNavigationBar(constraints),
-            Expanded(child: Container(color: Colors.white, child: page))
-          ],
-        ),
-      );
-    });
-  }
-
-  SafeArea getHorizontalNavigationBar(BoxConstraints constraints) {
-    return SafeArea(
-        child: NavigationRail(
-      destinations: const [
-        NavigationRailDestination(icon: Icon(Icons.home), label: Text("Home")),
-        NavigationRailDestination(
-            icon: Icon(Icons.login), label: Text("Login")),
-        NavigationRailDestination(
-            icon: Icon(Icons.supervised_user_circle),
-            label: Text("List of friends")),
-        NavigationRailDestination(icon: Icon(Icons.chat), label: Text("Chat")),
-      ],
-      selectedIndex: selectedIndex,
-      onDestinationSelected: (value) {
-        setState(() {
-          selectedIndex = value;
-        });
-      },
-      extended: constraints.maxWidth >= 700,
-      backgroundColor: Colors.lime.shade300,
-    ));
   }
 }
--- SFS_FrontEnd/lib/pages/GuestRoute/login.dart
+++ SFS_FrontEnd/lib/pages/GuestRoute/login.dart
@@ -1,38 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_login/flutter_login.dart';
+import 'package:sfs_frontend/pages/LoggedInRoute/loggedin_route.dart';
 import 'package:sfs_frontend/services/user_controller.dart';
 import 'package:sfs_frontend/services/user_state.dart';
 
-// For testing only
-const users = {
-  'nva': '12345',
-  'nvb': 'hunter',
-};
-
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
-
-  // Duration get loginTime => const Duration(milliseconds: 2250);
-  // Future<String?> _authUser(LoginData data) {
-  //   debugPrint('Name: ${data.name}, Password: ${data.password}');
-  //   return Future.delayed(loginTime).then((_) {
-  //     if (!users.containsKey(data.name)) {
-  //       return 'User not exists';
-  //     }
-  //     if (users[data.name] != data.password) {
-  //       return 'Password does not match';
-  //     }
-  //     return null;
-  //   });
-  // }
 
   @override
   Widget build(BuildContext context) {
     var userState = context.watch<UserState>();
     var userController = UserController(userState);
     return FlutterLogin(
-      onLogin: userController.signIn, 
+      onLogin: userController.dummySignIn, // TODO: Replace with  userController.signIn
       onRecoverPassword: (_){},
       onSignup: userController.signUp,
       logo: AssetImage("logo/logo_icon.png"),
@@ -49,7 +30,10 @@ class LoginPage extends StatelessWidget {
       userValidator: (_) => null,
       onSubmitAnimationCompleted:() {
         print("Logged ${userState.getUsername()} in successfully");
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => const LoggedInRoute(title: "Logged In"))
+        );
       },
-      );
+    );
   }
 }
