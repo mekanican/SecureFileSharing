3603ef54e79614f11caaf0a408c3799fda0fa66d
mekanican <boommerang73@gmail.com>
2023-05-29 23:34:28 +0700

    set some change on filesharing

--- backend/apps/file_sharing/serializers/filesharing.py
+++ backend/apps/file_sharing/serializers/filesharing.py
@@ -1,18 +1,18 @@
-from rest_framework import serializers;
-from ..models.filesharing import FileSharing
+from rest_framework import serializers
+
 from ...users.models import User
+from ..models.filesharing import FileSharing
+
 
 class FileSharingSerializer(serializers.ModelSerializer):
- 
     class Meta:
         model = FileSharing
-        fields = ["id", "username", "filename","myfile"]
+        fields = ["from_user", "to_user", "url", "uploaded_at"]
 
-    def validate_username( value):
+    def validate_username(value):
         """Validate password.
 
         Validate the password against the password validation policy.
 
         """
         return len(User.objects.filter(username=value))
-       
