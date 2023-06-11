2515f32f65dd13e413531115f600f8c0a0342aff
Loc Nguyen <loc.nguyen@saritasa.com>
2023-06-11 16:27:10 +0700

    Add keys getting API & modify key API endpoints

--- backend/apps/keys_handler/urls.py
+++ backend/apps/keys_handler/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import RSAKeyGenerateView
+from .views import RSAKeyGenerateView, RSAPublicKeyGetAPIView
 
 urlpatterns = [
-    path("generate/", RSAKeyGenerateView.as_view(), name="generate-keys"),
+    path("generate", RSAKeyGenerateView.as_view(), name="generate-keys"),
+    path("get", RSAPublicKeyGetAPIView.as_view(), name="get-public-key"),
 ]
