fb189f100a0b390e2b6823dcd161149425d8ff43
mekanican <boommerang73@gmail.com>
2023-06-12 02:46:41 +0700

    Adding/Refining "some" controller

---
+++ SFS_FrontEnd/lib/helper/file_handler.dart
@@ -0,0 +1,72 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:dio/dio.dart';
+
+import 'package:path_provider/path_provider.dart';
+import 'package:sfs_frontend/helper/rsa.dart';
+
+Future<String> get _localPath async {
+  final directory = await getApplicationSupportDirectory();
+  return directory.path;
+}
+
+const String pubkeyName = "/key.pub";
+const String privkeyName = "/key.priv";
+
+Future<bool> checkExistKeyPair() async {
+  final base = await _localPath;
+  var a = File(base + pubkeyName);
+  var b = File(base + privkeyName);
+
+  return await a.exists() && await b.exists();
+}
+
+Future<RSAPublicKey> getPubKey() async {
+  final base = await _localPath;
+  var a = File(base + pubkeyName);
+
+  var data = await a.readAsString();
+  Map<String, String> json = jsonDecode(data);
+  return RSAPublicKey.fromJSON(json);
+}
+
+Future<RSAPrivateKey> getPrivKey() async {
+  final base = await _localPath;
+  var a = File(base + privkeyName);
+  var data = await a.readAsString();
+  Map<String, String> json = jsonDecode(data);
+  return RSAPrivateKey.fromJSON(json);
+}
+
+void setPubKey(RSAPublicKey p) async {
+  final base = await _localPath;
+  var a = File(base + pubkeyName);
+
+  var data = jsonEncode(p.toJson());
+  a.writeAsString(data);
+}
+
+void setPrivKey(RSAPrivateKey p) async {
+  final base = await _localPath;
+  var a = File(base + privkeyName);
+
+  var data = jsonEncode(p.toJson());
+  a.writeAsString(data);
+}
+
+
+
+
+Future<void> downloadFile(String url, String filename) async {
+  var base = await _localPath;
+  // Create a Dio instance
+  var dio = Dio();
+
+  // Start the download
+  try {
+    await dio.download(url, "$base/$filename");
+    print('Download complete!');
+  } catch (e) {
+    print('Error downloading file: $e');
+  }
+}
---
+++ SFS_FrontEnd/lib/models/chat.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
+
+class ChatMessage {
+  final int from_id;
+  final int to_id;
+  final String url;
+  final DateTime createdAt;
+
+  ChatMessage({
+    required this.from_id,
+    required this.to_id,
+    required this.url,
+    required this.createdAt,
+  });
+
+  String _url2filename() {
+    // https://stackoverflow.com/a/56258202
+    RegExp r = RegExp(r"(?<=\/)[^\/\?#]+(?=[^\/]*$)");
+    RegExpMatch? match = r.firstMatch(url);
+    return match![0]!;
+  }
+
+  types.FileMessage toFileMessage() {
+    return types.FileMessage(
+      author: types.User(id: from_id.toString()), 
+      id: to_id.toString(), 
+      name: _url2filename(), 
+      size: 1234, 
+      uri: url
+    );
+  }
+}
