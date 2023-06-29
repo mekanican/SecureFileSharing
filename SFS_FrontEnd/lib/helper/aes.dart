d9ea2224fedf37078fb29ebf04fc1fe208436af4
mekanican <boommerang73@gmail.com>
2023-06-29 20:07:30 +0700

    hotfix

--- SFS_FrontEnd/lib/helper/aes.dart
+++ SFS_FrontEnd/lib/helper/aes.dart
@@ -7,15 +7,15 @@ class AESWrapper {
   static Bytes encrypt(Bytes data, Bytes key) {
     final encrypter = Encrypter(AES(Key(key), mode: AESMode.cbc));
     final iv = IV.fromLength(16);
-    final encrypted = encrypter.encrypt(utf8.decode(data), iv: iv);
+    final encrypted = encrypter.encryptBytes(data, iv: iv);
     return encrypted.bytes;
   }
 
   static Bytes decrypt(Bytes enc, Bytes key) {
     final decrypter = Encrypter(AES(Key(key), mode: AESMode.cbc));
     final iv = IV.fromLength(16);
-    final data = decrypter.decrypt(Encrypted(enc), iv: iv);
-    return Bytes.fromList(utf8.encode(data));
+    final data = decrypter.decryptBytes(Encrypted(enc), iv: iv);
+    return Bytes.fromList(data);
   }
 
 }
